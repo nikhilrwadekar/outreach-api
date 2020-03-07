ba7af59791cdb8d17de8ef3eb5f23af5e253d176
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-03-07 12:22:42 -0800

    Added basic Routes, Controllers

--- src/models/user.example.model.js
+++ src/models/user.example.model.js
@@ -1,1 +1,28 @@
-let user = {};
+let user = {
+  name: "Nikhil Wadekar",
+  email: "nikhilrwadekar@gmail.com",
+  password: "password",
+  // Should be a Buffer to Store the Image!
+  profile_picture_url: "",
+  contact_number: 9876543210,
+  role: "volunteer",
+  tasks: {
+    type: ["Mixed"]
+  },
+
+  address: {
+    street: "157, W 49th Ave",
+    city: "Vancouver",
+    country: "Canada",
+    province: "British Columbia",
+    postal_code: "V5Y2Z7"
+  },
+  preferences: {
+    volunteering_type: ["Cooking", "Driving"],
+    additional_skills: ["Consoling"]
+  },
+  availability: {
+    type: "anytime",
+    schedule: []
+  }
+};
