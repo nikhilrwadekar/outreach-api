edf108a06ea3364887b6c9810d78643a605bf0bd
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-03-07 11:22:11 -0800

    Fixed Route to Create: Relief Center

--- src/routes/api/relief-center.js
+++ src/routes/api/relief-center.js
@@ -7,15 +7,20 @@ const validator = require("express-validation");
 
 // Relief Center Controller and Validators
 const reliefCenterController = require("../../controllers/relief-center.controller");
-const { create } = require("../../validators/relief-center");
+const { createReliefCenter } = require("../../validators/relief-center");
 
 // Routes for Relief Centers
+
+// Get all Relief Centers
 router.get("/", reliefCenterController.getAllReliefCenters);
+
+// Create New Relief Center - Check if it exists based on it's name
 router.post(
   "/create",
-  validator(create),
+
   reliefCenterController.createReliefCenter
-); // Create New Relief Center - Check if it exists based on it's name
+);
+
 router.get("/id/:id", reliefCenterController.getReliefCenterByID); // Get a Relief Center based on it's ID
 router.get("/:name", reliefCenterController.getReliefCenterByName); // Get a Relief Center based on it's name
 
